eb742abf94921fbf502cf9bc545aac273fd36036
Felix Angell <felix.angell@deliveroo.co.uk>
2022-12-14 12:39:56 +0000

    day one

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,4 +1,36 @@
 from aocd.models import Puzzle
 
 d1 = Puzzle(year=2022, day=1)
-print(d1.input_data)
+
+def part_one():
+    calorie_accum = 0
+    largest_accum_calorie = 0
+
+    for line in d1.input_data.split('\n'):
+        if line == '':
+            calorie_accum = 0
+            continue
+        
+        calorie_accum += int(line)
+        largest_accum_calorie = max(calorie_accum, largest_accum_calorie)
+    
+    return largest_accum_calorie
+
+def part_two():
+    calorie_accum = 0
+    ca = []
+
+    for line in d1.input_data.split('\n'):
+        if line == '':
+            ca.append(calorie_accum)
+            calorie_accum = 0
+            continue
+
+        calorie_accum += int(line)
+    
+    ca = sorted(ca)
+    
+    return ca.pop() + ca.pop() + ca.pop()
+
+print(part_one())
+print(part_two())
